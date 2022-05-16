8f41d63e8c11cdbdc67774427ecb7fe36d7cf713
Juan Carlos Gutierrez <jc.gutierrezs@gmail.com>
2022-05-16 07:14:59 -0400

    falta ocultar apikey

--- prueba_nasa.rb
+++ prueba_nasa.rb
@@ -22,38 +22,41 @@ def build_web_page(principal)
     url_photo = photos.map{|x| x['img_src']}
     #print url_photo
     html = ""
-    html += "<html>\n<head>\n<title></title>\n</head>\n<body>\n<ul>\n"
-    url_photo.each do |photo|
-        html += "   <li><img src=\"#{photo}\" width='300' height='200'></li>\n" 
+    html += "<html>\n<head>\n<title></title>\n</head>\n<body>\n   <ul>\n"
+    url_photo[0..9].each do |photo|
+        html += "     <li><img src=\"#{photo}\"></li>\n" 
     end
-    html += "</ul>\n</body>\n</html>"
+    html += "   </ul>\n</body>\n</html>"
     File.write('index.html', html)
-  
 
+end
+
+def cont_photos(principal)
+    contador = {}
+    principal["photos"].each do |camera_name|
+        camera_name["camera"].each do |key,value|
+            if 
+                key == "name"
+                if
+                    contador.include? value
+                    contador[value] += 1
+                else
+                    contador[value] = 1
+                end
+            end
+        end
+    end
+    contador
 end
 
 
 principal = request("https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=DCQZgXMlvJjgCGJi8oWMtsotSEvs9PP8egenN3NN")
 build_web_page(principal)
-
+cont_photos(principal)
     
     
 
 
 
-  
 
-#  contents = <<~HTML
-#     <html>
-#       <head>
-#       </head>
-#       <body>
-#       <ul>
-#          <li>
-        
-#       </ul>
-#       </body>
-#     </html>
-#   HTML
 
-#   File.write("index.html", contents)
